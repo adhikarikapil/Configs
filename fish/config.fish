9ddc0be8bfedb9c524ac86a8034edf911ac8250d
Kapil Adhikari <kapiladhikari2060@gmail.com>
2023-08-14 23:20:35 +0545

    Updated Config

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+	set fish_greeting  #Supress fish's into message
+end
+
+#This is the only problem I had for now so I supressed it.
+#I will slowly change and update the configs after getting a hang of it.
